bd1a6d831075feea98019b2a0d1909f1f1ed443c
Yber-M <ybermarlon@gmail.com>
2024-06-29 01:40:48 -0500

    Se agrego un bucle y un ayuda para que el usuario adivine el número

--- Clase01/app.js
+++ Clase01/app.js
@@ -1,5 +1,3 @@
-let numAdivinar = 3;
-let respuesta = false;
 
 console.log("Hola Mundo"); //? Mensaje informativo
 console.error("Ocurrio un error"); //? Mensaje de error
@@ -11,18 +9,28 @@ const usuarios = [
 ];
 console.table(usuarios); //? Imprime datos en forma de tabla
 //? Este code hara que si o si ingrese
-while (respuesta == false) {
-  let rspt = prompt("Por favor indicar un número entre el 1 y 10:");
+
+let numAdivinar = 7;
+let rspt = 0;
+let numIntentos = 1;
+let palabraVeces = 'vez';
+while (rspt != numAdivinar) {
+  rspt = prompt("Por favor indicar un número entre el 1 y 10:");
   if (rspt <= 10 && rspt >= 1) {
     if (rspt == numAdivinar) {
       //OjO: Al usar tilde invertida podemos ingresar directamente la variable
-      alert(`Genial! El número ${rspt} es el correcto.`);
-      respuesta = true;
+      alert(`Genial! El número ${rspt} es el correcto. Tu N° de intentos fue: ${numIntentos} ${palabraVeces}`);
     } else {
-      alert("Numero incorrecto");
-      respuesta = false;
+      if (rspt > numAdivinar) {
+        alert('El número secreto es menor')
+
+      } else {
+        alert('El número secreto es mayor')
+      }
     }
   } else {
     alert('El número debe ser entre 1 y 10. Intente nuevamente');
   }
+  palabraVeces = 'veces';
+  numIntentos++;
 }
