6ccb670a0d2ff2c588edae0635a7fb87232c3e36
Yber-M <ybermarlon@gmail.com>
2024-07-01 11:36:21 -0500

    Se hace uso del operador ternario

--- Clase01/app.js
+++ Clase01/app.js
@@ -1,14 +1,14 @@
 
-console.log("Hola Mundo"); //? Mensaje informativo
-console.error("Ocurrio un error"); //? Mensaje de error
-console.warn("Esto es una advertencia"); //? Mensaje de advertencia
-console.info("Mensaje importante"); //? Similar que log
-const usuarios = [
-  { nombre: "Alice", edad: 25 },
-  { nombre: "Bob", edad: 30 },
-];
-console.table(usuarios); //? Imprime datos en forma de tabla
-//? Este code hara que si o si ingrese
+// console.log("Hola Mundo"); //? Mensaje informativo
+// console.error("Ocurrio un error"); //? Mensaje de error
+// console.warn("Esto es una advertencia"); //? Mensaje de advertencia
+// console.info("Mensaje importante"); //? Similar que log
+// const usuarios = [
+//   { nombre: "Alice", edad: 25 },
+//   { nombre: "Bob", edad: 30 },
+// ];
+// console.table(usuarios); //? Imprime datos en forma de tabla
+// //? Este code hara que si o si ingrese
 
 let numAdivinar = 7;
 let rspt = 0;
@@ -18,7 +18,7 @@ while (rspt != numAdivinar) {
   rspt = prompt("Por favor indicar un número entre el 1 y 10:");
   if (rspt <= 10 && rspt >= 1) {
     if (rspt == numAdivinar) {
-      alert(`Genial! El número ${rspt} es el correcto. Tu N° de intentos fue: ${numIntentos} ${palabraVeces}`);
+      alert(`Genial! El número ${rspt} es el correcto. Tu N° de intentos fue: ${numIntentos} ${palabraVeces == 1 ? 'vez' : 'veces'}`);
     } else {
       if (rspt > numAdivinar) {
         alert('El número secreto es menor')
