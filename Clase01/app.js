d898c70a45ca6348f1d66be78a8faea3ddfb9c9d
Yber-M <ybermarlon@gmail.com>
2024-07-02 22:03:18 -0500

    JuegoAdivinaNumeroAleatorio v1

--- Clase01/app.js
+++ Clase01/app.js
@@ -10,12 +10,15 @@
 // console.table(usuarios); //? Imprime datos en forma de tabla
 // //? Este code hara que si o si ingrese
 
-let numAdivinar = 7;
+// OjO -> Agregamos la funcionalidad para que de numeros al azar
+let numAdivinar = Math.floor(Math.random() * 10) + 1;
+console.log(numAdivinar); //? Imprimimos el número pseudo-aleatorio en consola
 let rspt = 0;
 let numIntentos = 1;
 let palabraVeces = 'vez';
 while (rspt != numAdivinar) {
-  rspt = prompt("Por favor indicar un número entre el 1 y 10:");
+  rspt = parseInt(prompt("Por favor indicar un número entre el 1 y 10:"));
+  console.log(typeof (rspt));
   if (rspt <= 10 && rspt >= 1) {
     if (rspt == numAdivinar) {
       alert(`Genial! El número ${rspt} es el correcto. Tu N° de intentos fue: ${numIntentos} ${palabraVeces == 1 ? 'vez' : 'veces'}`);
@@ -31,7 +34,7 @@ while (rspt != numAdivinar) {
     alert('El número debe ser entre 1 y 10. Intente nuevamente');
   }
   numIntentos++;
-  if (numIntentos > 3) {
+  if (numIntentos > 4) {
     alert('Superaste el tus 3 intentos :(');
     break; // OjO -> Acá marcamos el fin del bucle
   }
