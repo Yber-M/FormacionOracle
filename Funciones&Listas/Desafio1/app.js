3bd997c81b52fa5e846aa161ad7ce9d76bcb4f42
Yber-M <ybermarlon@gmail.com>
2024-07-05 22:44:02 -0500

    Desafio 1

---
+++ Funciones&Listas/Desafio1/app.js
@@ -0,0 +1,27 @@
+// OjO -> 2. Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".
+let texto = document.querySelector('h1');
+texto.innerHTML = 'Hora del Desafio'
+
+// OjO -> 3. Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".
+function btnConsole() {
+    console.log('El boton fue clicado');
+}
+
+// OjO -> 4. Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
+function btnMsgPrompt() {
+    rspt = prompt('Menciona el nombre de una ciudad de Perú');
+    alert(`Estuve en ${rspt} y me acordé de tí.`);
+}
+
+// OjO -> Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".
+function btnIloveJS() {
+    alert('Yo amo JS');
+}
+
+// OjO -> Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta.
+function sumaDosNum() {
+    let num1 = parseInt(prompt('Ingrese el primer número'));
+    let num2 = parseInt(prompt('Ingrese el segundo número'));
+    let resultado = num1 + num2;
+    alert(`El resultado de la suma de los números ${num1} + ${num2} = ${resultado}`)
+}
