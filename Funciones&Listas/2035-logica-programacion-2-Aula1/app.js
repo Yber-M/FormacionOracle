1a9eba5731b65fc3728ba1e2eecf9c3bb67d7009
Yber-M <ybermarlon@gmail.com>
2024-07-15 18:27:02 -0500

    Se agrego funcionalidad al boton reiniciar

--- Funciones&Listas/2035-logica-programacion-2-Aula1/app.js
+++ Funciones&Listas/2035-logica-programacion-2-Aula1/app.js
@@ -1,6 +1,8 @@
-let numSecret = generarNumSecret();
+let numSecret = 0;
+let numIntentos = 0;
 
-console.log(numSecret);
+
+condicionesInciales();
 
 function generarNumSecret() {
     return Math.floor(Math.random() * 10) + 1;
@@ -12,17 +14,39 @@ function asignarTexto(elemento, texto) {
     return;
 }
 
+function vaciarEntrada() {
+    document.querySelector('#varlorUsuario').value = '';
+}
 
 function verificarIntento() {
     let numInput = parseInt(document.getElementById('varlorUsuario').value);
+    console.log('Numero de intentos = ' + numIntentos);
     if (numInput === numSecret) {
-        asignarTexto('p', 'MUY BIEN. Adivinaste correctamente el número')
-    } else {
-        asignarTexto('p', 'VUELVE A INTENTARLO. Ingreso un número del 1 al 100');
+        asignarTexto('p', `MUY BIEN. Adivinaste el número en ${numIntentos} ${(numIntentos === 1) ? 'vez' : 'veces'}`)
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }
+    else {
+        if (numInput > numSecret) {
+            asignarTexto('p', 'El número es menor')
+        } else {
+            asignarTexto('p', 'El número es mayor')
+        }
+        numIntentos++;
+        vaciarEntrada();
     }
     return;
 }
 
+function reiniciarGamer() {
+    vaciarEntrada();
+    condicionesInciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
 
-asignarTexto('h1', 'Juego del número secreto');
-asignarTexto('p', 'Ingreso un número del 1 al 100');
+function condicionesInciales() {
+    asignarTexto('h1', 'Juego del número secreto');
+    asignarTexto('p', 'Ingreso un número del 1 al 100');
+    numSecret = generarNumSecret();
+    numIntentos = 1;
+    console.log(numSecret);
+}
