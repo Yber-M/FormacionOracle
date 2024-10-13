4b2c9b08f5ae2d1563c1548ceb78df6092f6f18b
Yber-M <ybermarlon@gmail.com>
2024-10-13 15:54:33 -0500

    ⚡Se mejoro el algoritmo: se usaron funciones y se manejo el DOM de manera optima

--- AluraMidi/main.js
+++ AluraMidi/main.js
@@ -1,7 +1,21 @@
-function playSonidoPom() {
-    document.querySelector('#sonido_tecla_pom').play();
+function playSonido(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = playSonidoPom;
+
+let cont = 0;
+
+while (cont < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[cont];
+    const sonidoSeleccionado = tecla.classList[1];
+    const idAudio = `#sonido_${sonidoSeleccionado}`;
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        playSonido(idAudio);
+    };
+
+    cont++;
+}
