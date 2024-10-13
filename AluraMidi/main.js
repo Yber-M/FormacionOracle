15ac4620552556b0c1f861af6678b46581bb92a6
Yber-M <ybermarlon@gmail.com>
2024-10-13 16:54:09 -0500

    ♿️ Mejora de la Accesibilidad: Se arreglo el error al presionar los sonidos con las teclas space y enter

--- AluraMidi/main.js
+++ AluraMidi/main.js
@@ -9,9 +9,19 @@ for (let cont = 0; cont < listaDeTeclas.length; cont++) {
     const tecla = listaDeTeclas[cont];
     const sonidoSeleccionado = tecla.classList[1];
     const idAudio = `#sonido_${sonidoSeleccionado}`;
-    console.log(idAudio);
 
     tecla.onclick = function () {
         playSonido(idAudio);
     };
+
+    tecla.onkeydown = function (event) {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('activa');
+        }
+    };
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('activa');
+    }
+
 }
