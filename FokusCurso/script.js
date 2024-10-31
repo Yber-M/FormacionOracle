bca89de6a870db078f17ef4a0d9ff00920aa751c
Yber-M <ybermarlon@gmail.com>
2024-10-31 14:36:07 -0500

    ✨ Cambiando de texto | Se agrego  la funcionalidad de cambiar el texto cada que se  hace click

--- FokusCurso/script.js
+++ FokusCurso/script.js
@@ -1,11 +1,38 @@
 const html = document.querySelector('html');
 const btnCorto = document.querySelector('.app__card-button--corto');
 const btnEnfoque = document.querySelector('.app__card-button--enfoque');
+const btnLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 btnCorto.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-corto');
+    cambiarContexto('descanso-corto');
 })
 
 btnEnfoque.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'enfoque');
+    cambiarContexto('enfoque');
 })
+
+btnLargo.addEventListener('click', () => {
+    cambiarContexto('descanso-largo');
+})
+
+function cambiarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `./imagenes/${contexto}.png`)
+
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `Optimiza tu productividad,<br>
+            <strong class="app__title-strong">sumérgete en lo que importa.</strong>`;
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `¿Qué tal tomar un respiro?<br>
+            <strong class="app__title-strong">¡Haz una pausa corta!.</strong>`;
+            break;
+        case "descanso-largo":
+            titulo.innerHTML = `Hora de volver a la superficie<br>
+            <strong class="app__title-strong">¡Haz una pausa larga!.</strong>`
+            break;
+    }
+}
