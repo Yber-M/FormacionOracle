734034c2b0d66b534f48bb581dbf67f2b829e33a
Yber-M <ybermarlon@gmail.com>
2024-11-21 15:38:01 -0500

    ✅ Se cambio todas las imagenes haciendo uso de la API de la NASA

--- SpaceApp/script.js
+++ SpaceApp/script.js
@@ -1,12 +1,28 @@
-const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=20"
+const url = "https://api.nasa.gov/planetary/apod?api_key=ryasrRo4FiO9ju0cz3dLZTpS9Kq2VV8viPFYvLuQ&count=50"
 
-function listarImg() {
-    fetch(url)
-        .then(Response => Response.json())
-        .then(datosImg => {
-            console.log(datosImg);
-        })
-        .catch()
+async function listarImagenes() {
+    try {
+        let fetcImg = await fetch(url);
+        let dataImg = await fetcImg.json();
+
+        const card = document.querySelector('[data-ul]');
+        const banner = document.querySelector('.banner');
+
+        const bannerContent = `<img class="banner__image" src="${dataImg[0].url}" alt="bla">`
+        banner.innerHTML = banner.innerHTML + bannerContent;
+
+
+        dataImg.forEach(element => {
+            const contenido = `<li class="card">
+                            <img class="card__image" src="${element.url}" alt="imagen">
+                            <h3 class="card__title">${element.title}</h3>
+                            </li>`
+
+            card.innerHTML = card.innerHTML + contenido;
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 }
-
-listarImg();
+listarImagenes();
