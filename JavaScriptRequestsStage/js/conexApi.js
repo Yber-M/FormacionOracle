5986b75dbccb8738afe303647d09307611b4790a
Yber-M <ybermarlon@gmail.com>
2024-11-24 15:39:56 -0500

    Se implementa la logica para poder indexar un nuevo video

--- JavaScriptRequestsStage/js/conexApi.js
+++ JavaScriptRequestsStage/js/conexApi.js
@@ -3,11 +3,27 @@ async function listaVideo() {
 
     const conexionConvert = conexion.json();
 
-    return conexionConvert;    
+    return conexionConvert;
+}
+
+async function crearVideo(titulo, descripcion, url, imagen) {
+    const conexion = await fetch('http://localhost:3001/videos', {
+        method: 'POST',
+        headers: { 'Content=type': 'application/json' },
+        body: JSON.stringify({
+            titulo: titulo,
+            descripcion: descripcion,
+            url: url,
+            imagen: imagen
+        })
+    });
+    const conexionConvert = conexion.json();
+
+    return conexionConvert;
 }
 
 export const conexApi = {
-    listaVideo
+    listaVideo, crearVideo
 }
 
 listaVideo();
